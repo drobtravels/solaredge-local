940eda8374cd97d8e7414e207817ae4a2bffcdc7
David Roberts <drob@drob.net>
2019-03-31 16:24:18 -0400

    initial publish

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 import io
 import codecs
 import os
@@ -21,46 +20,28 @@ def read(*filenames, **kwargs):
 
 long_description = read('README.md')
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
-
 setup(
     name='solaredge_local',
-    version=solaredge-local.__version__,
+    version="0.1.0",
     url='https://github.com/drobtravels/solaredge-local',
     license='MIT License',
     author='David Roberts',
-    tests_require=['pytest'],
-    install_requires= [],
-    cmdclass={'test': PyTest},
-    author_email='',
-    description='Automated REST APIs for existing database-driven systems',
+    author_email="",
+    description='API wrapper to communicate locally with SolarEdge Inverters',
     long_description=long_description,
-    packages=['solaredge-lcoal'],
+    packages=['solaredge_local'],
     include_package_data=True,
     platforms='any',
-    test_suite='solaredge_local.test.test_api',
+    install_requires=[
+        'uplink',
+        'uplink-protobuf'
+    ],
     classifiers = [
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Development Status :: 4 - Beta',
         'Natural Language :: English',
-        'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        ],
-    extras_require={
-        'testing': ['pytest'],
-    }
+    ],
 )
