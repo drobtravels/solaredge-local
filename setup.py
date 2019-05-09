f324e8c6f050878458182063f2b41adddf19f8fd
David Roberts <drob@drob.net>
2019-05-08 20:27:10 -0400

    bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ long_description = read('README.md')
 
 setup(
     name='solaredge_local',
-    version="0.1.3",
+    version="0.1.4",
     url='https://github.com/drobtravels/solaredge-local',
     license='MIT License',
     author='David Roberts',
