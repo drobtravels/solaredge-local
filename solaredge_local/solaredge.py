23296b6e04ba9a8890ea08082786a38b2c06b0c9
RE1992 <re161992@gmail.com>
2019-05-20 16:02:27 +0100

    Add information.proto for information endpoint.

--- solaredge_local/solaredge.py
+++ solaredge_local/solaredge.py
@@ -1,6 +1,7 @@
 from uplink import Consumer, get
 from .maintenance_pb2 import Maintenance
 from .status_pb2 import Status
+from .information_pb2 import Information
 
 class SolarEdge(Consumer):
     @get("/web/v1/status")
@@ -13,4 +14,8 @@ class SolarEdge(Consumer):
 
     @get("/web/v1/maintenance")
     def get_maintenance(self) -> Maintenance:
+        pass
+
+    @get("/web/v1/information")
+    def get_information(self) -> Information:
         pass
