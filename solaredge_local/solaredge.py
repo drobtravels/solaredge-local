7f5f1e369ad8463835bcc37d3f77e6aadd7d0dbc
RE1992 <re161992@gmail.com>
2019-05-20 20:57:30 +0100

    Add powercontrol

--- solaredge_local/solaredge.py
+++ solaredge_local/solaredge.py
@@ -2,6 +2,7 @@ from uplink import Consumer, get
 from .maintenance_pb2 import Maintenance
 from .status_pb2 import Status
 from .information_pb2 import Information
+from .powercontrol_pb2 import PowerControl
 
 class SolarEdge(Consumer):
     @get("/web/v1/status")
@@ -18,4 +19,8 @@ class SolarEdge(Consumer):
 
     @get("/web/v1/information")
     def get_information(self) -> Information:
+        pass
+
+    @get("/web/v1/power_control")
+    def get_power_control(self) -> PowerControl:
         pass
